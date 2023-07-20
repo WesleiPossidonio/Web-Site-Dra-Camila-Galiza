13a06ffb5e0e72db8a74881bf47542cb3d60457d
Weslei França <weslwifranca@gmail.com>
2023-07-20 16:35:57 -0300

    update pages home

--- assets/js/instagram.js
+++ assets/js/instagram.js
@@ -59,30 +59,38 @@ $(function () {
   .then(data => {
   const {token} = data
 
-  const url = `https://graph.instagram.com/me/media?access_token=${token}&fields=media_url,media_type,caption,permalink`;
+  const url = `https://graph.instagram.com/me/media?access_token=${token}&fields=media_url,media_type,caption,permalink&limit=15`;
   
   const imageLink = $('.instagram-image');
   const linkImageInsta = $('.instagram-link');
 
   
-  const storedDataJsonString = localStorage.getItem('instagramData');
-  const storedDataJson = JSON.parse(storedDataJsonString);
+  const storedDataJsonArrayString = localStorage.getItem('instagramData');
+  const storedDataJson = JSON.parse(storedDataJsonArrayString);
 
   $.get(url).then(function (response) {
     console.log('retorno:', response.data);
 
     const dataJson = response.data;
+   
+
+    const imgUrl = dataJson.filter(data => data.media_type === 'IMAGE');
+    const imgUrlCaroulsel = dataJson.filter(data => data.media_type === 'CAROUSEL_ALBUM');
+    
+    const listImages = [ ...imgUrlCaroulsel,  ...imgUrl]
+
 
     // Comparar os dados recebidos da API com os dados armazenados
-    const isDataUpdated = JSON.stringify(dataJson) !== storedDataJsonString;
+    const isDataUpdated = JSON.stringify(listImages) !== storedDataJsonArrayString;
 
     // Atualizar os dados armazenados se houver alterações
     if (isDataUpdated) {
-      const dataJsonString = JSON.stringify(dataJson);
+      const dataJsonString = JSON.stringify(listImages);
       localStorage.setItem('instagramData', dataJsonString);
     }
 
     storedDataJson.forEach((data, index) => {
+      
       const imageUrl = data.media_url;
       const postLink = data.permalink;
   
