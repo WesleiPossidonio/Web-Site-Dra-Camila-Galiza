d430b39bc3dafd479ce19053d43c4ef03b64b6fe
Weslei França <weslwifranca@gmail.com>
2023-08-08 14:06:30 -0300

    update js

--- assets/js/instagram.js
+++ assets/js/instagram.js
@@ -1,49 +1,3 @@
-// var feed = new Instafeed({
-//   get: 'user',
-//   userId: '49698855873',
-//   accessToken: 'IGQVJWUWhIeG1hTFY4N3daaXFqN3BFeGpZAN0ZAyRnJaZADUtOHdRWTE3U2d0VGpRZAUdHQzJoYjBDNW5CUDNKZAG5oQ3MzY01jUUZAvRkEyNGEweS1OV1RWTkh1bHNEX1dWdGh6aEMtdF9wLWNEQXhoYXdUcwZDZD',
-//   limit: 6,
-//   template: `
-//   <div class="col-6 col-md-4">
-//   <a class="d-block instagram-link" href="" target="_blank">
-//       <figure class="overlay overlay-1 hover-scale rounded">
-//           <a href="" target="_blank">
-//               <img class=" object-fit-cover instagram-image" src="" alt="Instagram image" />
-//           </a>
-//           <figcaption class="d-flex align-items-center justify-content-center mt-5">
-//               <i class="uil uil-instagram z-index-2 lead text-white position-relative"></i>
-//           </figcaption>
-//       </figure>
-//   </a>
-// </div>`,
-// after: function () {
-//   // After the images are loaded, update the links and images
-//   var links = document.getElementsByClassName('instagram-link');
-//   var images = document.getElementsByClassName('instagram-image');
-//   var row = document.getElementById('instafeed-row');
-
-//   for (var i = 0; i < links.length; i++) {
-//     var link = this.data[i].link;
-//     var image = this.data[i].images.standard_resolution.url;
-
-//     // Check if the link or image already exists
-//     var isDuplicate = true;
-//     for (var j = 0; j < i; j++) {
-//       if (links[j].getAttribute('href') === link || images[j].getAttribute('src') === image) {
-//         isDuplicate = false;
-//         break;
-//       }
-//     }
-
-//     if (!isDuplicate) {
-//       links[i].setAttribute('href', link);
-//       images[i].setAttribute('src', image);
-//     }
-//   }
-// },
-// });
-
-// feed.run();
 
 // const apiUrl = 'https://back-end-site-dra-camila.vercel.app/token';
 
@@ -59,8 +13,9 @@ $(function () {
   // .then(data => {
   // const {token} = data
 
+  // console.log(token)
 
-  const url = `https://graph.instagram.com/me/media?access_token=IGQVJWWkYxUllrMHFJRTRqbTZApQl9IbkZAmU3ZABMW5GM1R2R2Y3elkxdHhUbXZA1Njg1WWhtT1ltYTMxdzc5QzY3ZA3BENFllUmFJaDRxcU93X1BhZADlMQ045czNiclZA3T3JRTm5wRHkybXotTHBWMXBvWgZDZD&fields=media_url,media_type,caption,permalink&limit=15`;
+  const url = `https://graph.instagram.com/me/media?access_token=IGQVJXNVRLTm5xVUlRdkM0eG53X3hqUEpBYTR1QXNhMWl4dW5yRzJaLVVKZAm9DT0c1cmZAPeTRCR2R0V0RXWjhOVmJrV0ppbmtxYm1yY1NoazBEQnRrYXdnMFhUZAElKUGpqdk9qaXBvdm5jeFpSaDlNeQZDZD&fields=media_url,media_type,caption,permalink&limit=15`;
   
   const imageLink = $('.instagram-image');
   const linkImageInsta = $('.instagram-link');
